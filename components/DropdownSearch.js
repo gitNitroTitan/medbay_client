808ab2a78bbe2944e4b91b1ef752f28b0bdee761
Jeremiah King <usaxray25@gmail.com>
2023-02-27 20:36:13 -0600

    userdropdown working--handlechange fixed to access target user

--- components/DropdownSearch.js
+++ components/DropdownSearch.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import { getAllRecords } from '../api/recordData';
+import { getAllRecords, getRecordsByUser } from '../api/recordData';
 import { getAllUsers } from '../api/userData';
 import RecordCardLite from './RecordCardLite';
 
@@ -10,40 +10,33 @@ function DropdownSearch() {
   const [users, setUsers] = useState([]);
   const [records, setRecords] = useState([]);
 
-  const [formInput, setFormInput] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormInput((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
   const getMedicationsByUser = () => {
-    getAllUsers().then(setUsers);
-    console.warn(users);
-    getAllRecords(users.id).then(setRecords);
+    getAllRecords().then(setRecords);
   };
 
   useEffect(() => {
+    getAllUsers().then(setUsers);
     getMedicationsByUser();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    getRecordsByUser(value).then(setRecords);
+  };
 
   return (
     <div className="searchbar">
       <InputGroup className="mb-3">
         <Form.Select className="mb-3" aria-label="User" name="userId" onChange={handleChange} required>
-          {records.user?.id ? <option value="">{records.user?.name}</option> : <option value="">Select User</option>}
+          {users.id ? <option value="">{users?.name[0]}</option> : <option value="">Select User</option>}
           {
-              records?.map((userTaco) => (
+              users?.map((userTaco) => (
                 <option
-                  key={userTaco.user.id}
-                  value={userTaco.user.id}
-                  defaultValue={userTaco.id === formInput.recordId}
+                  key={userTaco.id}
+                  value={userTaco.id}
                 >
-                  {userTaco.user.name}
+                  {userTaco.name}
                 </option>
               ))
           }
@@ -51,27 +44,11 @@ function DropdownSearch() {
       </InputGroup>
       <section className="search-medrecord-container">
         {records?.map((record) => (
-          <RecordCardLite key={record.user?.id} recordObj={record} />
+          <RecordCardLite key={record?.id} recordObj={record} />
         ))}
       </section>
     </div>
   );
 }
-
-// DropdownSearch.propTypes = {
-//   recordObj: PropTypes.shape({
-//     id: PropTypes.number,
-//     name: PropTypes.string,
-//     dosage: PropTypes.string,
-//     treatment: PropTypes.string,
-//     datePrescribed: PropTypes.string,
-//     userId: PropTypes.number,
-//     user: PropTypes.shape({
-//       id: PropTypes.number,
-//       name: PropTypes.string,
-//     }),
-//   }).isRequired,
-// setFilteredRecords: PropTypes.func.isRequired,
-// };
 
 export default DropdownSearch;
